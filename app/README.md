1ad7894cfc5fad86208d2b896f015e70db67be8e
Ethan Welborn <welborn.ethan@gmail.com>
2014-01-26 23:50:55 -0700

    trying to set up node environment. painful

--- app/README.md
+++
@@ -1,4 +0,0 @@
-hubbub
-======
-
-[![Code Climate](https://codeclimate.com/github/ethanwelborn/hubbub.png)](https://codeclimate.com/github/ethanwelborn/hubbub)
--- app/app.js
+++ app/app.js
@@ -1,24 +1,35 @@
 var express = require("express");
+var mongoose = require('mongoose');
  
 var app = express();
 app.use(express.logger());
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/app');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/app'));
-  app.use(app.router);
+app.configure(function() {
+	app.set('view engine', 'jade');
+	app.set('views', __dirname);
+	app.use(express.static(__dirname + 'app'));
 });
 
-app.get('/', function(request, response) {
-  response.render('index.haml')
+app.get('/', function(req, res) {
+  res.render('index.jade');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+mongoose.connect(uristring, function (err, res) {
+	if (err) {
+		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+	} else {
+		console.log ('Succeeded connected to: ' + uristring);
+}
 });

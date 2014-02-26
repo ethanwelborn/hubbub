9f501fce0e28d6a4cd9fbb8050f184ca4a06394b
Ethan Welborn <welborn.ethan@gmail.com>
2014-02-26 00:34:02 -0700

    working on interaction views

--- app/client/controllers/InteractionListCtrl.js
+++ app/client/controllers/InteractionListCtrl.js
@@ -3,14 +3,14 @@
 angular.module('hubbubApp')
   .controller('InteractionListCtrl', ['$scope', '$http', '$cookies', '$location', function ($scope, $http, $cookies, $location) {
 
-  	if (!$cookies.hubbub_loggedIn) {
+    if (!$cookies.hubbub_loggedIn) {
         $location.path('/');
     }
 
     $scope.interactions = {};
 
     $http.get('/api/v1/interactions/all/'+$cookies.hubbub_loggedIn)
-    	.success(function (data) {
-        	$scope.interactions = data;
-    	});
+      .success(function (data) {
+          $scope.interactions = data;
+      });
   }]);

da8ca9d722a6a5ce8ab977f2ea7f31867c703cb3
Ethan Welborn <welborn.ethan@gmail.com>
2014-04-05 01:06:19 -0600

    fixing date

--- app/client/controllers/InteractionListCtrl.js
+++ app/client/controllers/InteractionListCtrl.js
@@ -12,5 +12,17 @@ angular.module('hubbubApp')
     $http.get('/api/v1/interactions/all/'+$cookies.hubbub_loggedIn)
       .success(function (data) {
           $scope.interactions = data;
+          $scope.resolveDates();
       });
+
+    $scope.resolveDates = function() {
+      var interaction;
+      for (var i = 0; i < $scope.interactions.length; i++) {
+        interaction = $scope.interactions[i];
+        if (!interaction.timestamp) {
+          interaction.timestamp = '';
+        }
+        interaction.timestamp = new Date(interaction.timestamp.replace(/-/g,"/"));
+      }
+    }
   }]);
